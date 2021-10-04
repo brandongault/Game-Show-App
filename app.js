54dc9d895a792e151938c602f3287e6f876b2827
Brandon <brandongault8@gmail.com>
2021-10-03 20:41:43 -0400

    Added phrases, created random phrase generator

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
+// VARIABLES //
+
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const btn_reset = document.getElementByClassName('btn_reset');
+const startButton = document.getElementByClassName('btn_reset');
 let missed = 0;
+let randomPhrase = Math.floor( Math.random() * phrases.length );
+
+// ARRAY //
+let phrases [
+  // PHRASE 1 //
+  'We will cross that bridge when we come to it',
+  // PHRASE 2 //
+  'A penny for your thoughts',
+  // PHRASE 3 //
+  'Birds of a feather flock together',
+  // PHRASE 4 //
+  'Once in  a blue moon',
+  // PHRASE 5 //
+  'The early bird gets the worm',
+];
+
+// RETURN A RANDOM PHRASE FROM AN ARRAY //
+const getRandomPhraseAsArray = arr => {
+  
+}
+// ADDS THE LETTERS OF A STRING TO THE DISPLAY //
+const addPhraseToDisplay => {
+
+}
+// CHECK IF A LETTER IS IN THE PHRASE //
+const checkLetter = button => {
+
+}
+// CHECK IF THE GAME HAS BEEN WON OR LOST //
+const checkWin = () => {
+
+}
+// LISTEN FOR THE START GAME BUTTON TO BE PRESSED //
+startButton.addEventListener('click', () => {
+
+});
+// LISTEN FOR THE ONSCREEN KEYBOARD TO BE CLICKED //
+qwerty.addEventListener('click', e => {
+
+});
